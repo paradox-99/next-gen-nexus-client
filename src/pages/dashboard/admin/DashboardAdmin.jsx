634f577f79153e3eab847e9f7c605f2fa7eef5b8
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2024-06-09 02:13:35 +0600

    added coupon page. implemented coupon view, update and delete functionality

--- src/pages/dashboard/admin/DashboardAdmin.jsx
+++ src/pages/dashboard/admin/DashboardAdmin.jsx
@@ -1,8 +1,53 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const poppinsFont = createTheme({
+    typography: {
+        fontFamily: [
+            'Poppins',
+            'sans-serif',
+        ].join(','),
+        fontSize: 16,
+    },
+});
 
 const DashboardAdmin = () => {
+
+    const axiosPublic = useAxiosPublic();
+
+    const { data: counts } = useQuery({
+        queryKey: ['adminDashboard'],
+        queryFn: async () => {
+            const response = await axiosPublic.get("/adminDashboardData");
+            return response.data;
+        }
+    });
+
+    const data = [
+        { id: 0, value: counts?.usersCount, label: 'Total Users' },
+
+        { id: 1, value: counts?.productsCount, label: 'Total Product' },
+        { id: 2, value: counts?.reviewsCount, label: 'Total Review' },
+    ];
+
     return (
-        <div>
-            
+        <div className='flex justify-center items-center min-h-screen px-60'>
+            <ThemeProvider theme={poppinsFont}>
+                <PieChart
+                    series={[
+                        {
+                            data,
+                            highlightScope: { faded: 'global', highlighted: 'item' },
+                            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                            arcLabel: (item) => `${item.label} (${item.value})`,
+                        },
+                    ]}
+                    height={500}
+                    
+                />
+            </ThemeProvider>
         </div>
     );
 };
--- src/pages/dashboard/admin/ManageCoupon.jsx
+++ src/pages/dashboard/admin/ManageCoupon.jsx
@@ -1,12 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 // import useAuth from "../../../hooks/useAuth";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, createTheme } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, createTheme } from "@mui/material";
 import { MdDelete } from "react-icons/md";
 import toast from "react-hot-toast";
 import Title from "../../shared/Title";
 import { IoEyeOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
+import { useState } from "react";
+import { Add, CancelOutlined } from "@mui/icons-material";
+import { CustomTextField } from "../../../components/basic/basicComponents";
+import Swal from "sweetalert2";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const montserratFont = createTheme({
     typography: {
@@ -33,6 +38,10 @@ const ManageCoupon = () => {
 
     const axiosPublic = useAxiosPublic();
     // const { user } = useAuth();
+    const [open, setOpen] = useState(false);
+    const [isEditable, setEditable] = useState(false);
+    const [coup, setCoup] = useState();
+    const [ addOrUpdate, setAddOrUpdate] = useState(true);
 
     const { refetch, data: coupons } = useQuery({
         queryKey: ['coupons'],
@@ -42,16 +51,91 @@ const ManageCoupon = () => {
         }
     })
 
-    console.log(coupons);
+    const deleteCoupon = async (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await axiosPublic.delete(`/deleteCoupon/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
 
-    const deleteProduct = async (id, email) => {
-        await axiosPublic.delete(`/deleteProducts/product?id=`)
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const viewDetails = coupon => {
+        setCoup(coupon)
+        handleClickOpen();
+        setEditable(false);
+    }
+
+    const setEdit = () => {
+        setOpen(true);
+        setEditable(true);
+        setAddOrUpdate(!addOrUpdate);
+    }
+
+    const setEdit2 = (coupon) => {
+        setCoup(coupon)
+        handleClickOpen();
+        setEditable(true);
+        setAddOrUpdate(false);
+    }
+
+    const updateCoupon = async (id, info) => {
+        // await axiosPublic.put(`/updateCoupon/${id}`, info)
+        //     .then(res => {
+        //         if (res.data.modifiedCount > 0) {
+        //             toast.success("Updated successfully.")
+        //             refetch();
+        //             setOpen(false);
+        //         }
+        //     })
+        console.log("called update");
+
+    }
+
+    const openNewCoupon = () => {
+        setOpen(true);
+        setEditable(true);
+        setAddOrUpdate(true);
+        setCoup(null);
+    }
+
+    const addNewCoupon = async (info) => {
+        await axiosPublic.post('/addCoupon', info)
             .then(res => {
-                if (res.data.deletedCount > 0) {
-                    toast.success("Deleted successfully.")
+                if (res.data.insertedCount > 0) {
+                    toast.success("Added successfully.")
                     refetch();
+                    setOpen(false);
                 }
             })
+
+        console.log("called add new");
     }
 
     return (
@@ -76,7 +160,7 @@ const ManageCoupon = () => {
                             {coupons?.map((coupon) => (
                                 <TableRow
                                     hover
-                                    key={coupon.product_id}
+                                    key={coupon._id}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     <TableCell component="th" scope="row">
@@ -84,12 +168,13 @@ const ManageCoupon = () => {
                                     </TableCell>
                                     <TableCell align="left">{coupon.expiration_date}</TableCell>
                                     <TableCell align="left">{coupon.discount_amount}%</TableCell>
-                                    <TableCell align="left"><button className="text-2xl px-2 py-1 rounded"><IoEyeOutline /></button></TableCell>
-                                    <TableCell align="right">
-                                        <button className="px-2 py-1 text-2xl"><FaRegEdit /></button>
+                                    <TableCell align="left"><button onClick={() => viewDetails(coupon)} className="text-2xl px-2 py-1 rounded"><IoEyeOutline /></button>
                                     </TableCell>
                                     <TableCell align="right">
-                                        <button onClick={() => deleteProduct(coupon._id, coupon.product_owner_info.owner_email)} className="text-red-600 text-2xl px-2 py-1 rounded"><MdDelete />
+                                        <button onClick={() => setEdit2(coupon)} className="px-2 py-1 text-2xl"><FaRegEdit /></button>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <button onClick={() => deleteCoupon(coupon._id)} className="text-red-600 text-2xl px-2 py-1 rounded"><MdDelete />
                                         </button>
                                     </TableCell>
                                 </TableRow>
@@ -98,6 +183,110 @@ const ManageCoupon = () => {
                     </ThemeProvider>
                 </Table>
             </TableContainer>
+            <div className="absolute right-20 bottom-10">
+                <IconButton style={{ color: "black" }} onClick={openNewCoupon}>
+                    <IoIosAddCircleOutline className="text-4xl" />
+                </IconButton>
+            </div>
+            <Dialog
+                open={open}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        const coupon_code = formJson.code;
+                        const expiration_date = formJson.expiry_date;
+                        const coupon_description = formJson.description;
+                        const discount_amount = formJson.discount;
+
+                        const info = { coupon_code, expiration_date, coupon_description, discount_amount }
+                        if(coup._id)
+                            updateCoupon(coup?._id, info)
+                        else
+                            addNewCoupon(info)
+                    },
+                }}
+                maxWidth="md"
+            >
+                <div className="flex justify-between">
+                    <ThemeProvider theme={montserratFont}>
+                        <DialogTitle style={{ fontWeight: 600 }}>Details</DialogTitle>
+                    </ThemeProvider>
+                    <DialogActions>
+                        <Button onClick={handleClose} style={{ color: "black", fontSize: 16 }}><CancelOutlined /></Button>
+                    </DialogActions>
+                </div>
+                <ThemeProvider theme={poppinsFont}>
+                    <DialogContent>
+                        <div className="flex gap-5">
+                            <CustomTextField
+                                autoFocus
+                                required
+                                id="name"
+                                name="code"
+                                defaultValue={coup?.coupon_code}
+                                label="Coupon code"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                {...isEditable ? { disabled: false } : { disabled: true }}
+
+                            />
+                            <CustomTextField
+                                autoFocus
+                                required
+                                id="name"
+                                label='Expiry date'
+                                name="expiry_date"
+                                defaultValue={coup?.expiration_date}
+                                type="date"
+                                fullWidth
+                                variant="standard"
+                                {...isEditable ? { disabled: false } : { disabled: true }}
+                            />
+                            <CustomTextField
+                                autoFocus
+                                required
+                                id="name"
+                                name="discount"
+                                label="Discount amount"
+                                defaultValue={coup?.discount_amount}
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                {...isEditable ? { disabled: false } : { disabled: true }}
+                            />
+                        </div>
+                        <CustomTextField
+                            autoFocus
+                            required
+                            id="name"
+                            name="description"
+                            label="Coupon description"
+                            defaultValue={coup?.coupon_description}
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            multiline
+                            {...isEditable ? { disabled: false } : { disabled: true }}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <div className={`flex gap-10 ${isEditable ? 'block' : 'hidden'}`}>
+                            <button type="submit" onSubmit={onsubmit} className={`outline font-poppins outline-1 px-2 py-1 rounded ${isEditable ? 'block' : 'hidden'} hover:bg-[#D6EFFF]`}>{addOrUpdate ? "Add" : "Update"}</button>
+
+                            <button onClick={() => setEditable(false)} className={`outline font-poppins outline-1 px-2 py-1 rounded ${isEditable ? 'block' : 'hidden'} hover:bg-[#D6EFFF]`}>Cancel</button>
+                        </div>
+                        <div className={`${!isEditable ? 'block' : 'hidden'}`}>
+                            <button onClick={setEdit} {...!isEditable ? { disabled: false } : { disabled: true }} className="px-2 py-1 text-2xl"><FaRegEdit /></button>
+                            <button onClick={() => deleteCoupon()} className="text-red-600 text-2xl px-2 py-1 rounded"><MdDelete />
+                            </button>
+                        </div>
+                    </DialogActions>
+                </ThemeProvider>
+            </Dialog>
         </div>
     );
 };
